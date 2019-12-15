b7cd593ec90835a06ba09c4adb202428219e6fa7
David Tucker <david@globomantics.com>
2019-12-15 17:55:38 -0500

    Adding more tests

--- test/findPrimes.js
+++ test/findPrimes.js
@@ -6,7 +6,18 @@ const primes100 = [ 2,  3,  5,  7,
     41, 43, 47, 53, 59, 61, 67, 71,
     73, 79, 83, 89, 97 ];
 
+const primes4 = [ 2 ];
+
 describe('findPrimes()', function() {
+
+  context('max 4', function() {
+    it('should return 1 prime', function() {
+      expect(findPrimes(4).length).to.equal(1)
+    })
+    it('should return the correct primes', function() {
+        expect(findPrimes(4)).to.eql(primes4)
+      })
+  })
 
   context('max 100', function() {
     it('should return 25 primes', function() {
